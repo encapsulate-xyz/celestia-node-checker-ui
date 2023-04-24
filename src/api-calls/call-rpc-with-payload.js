6149d739bd49b7c94d95c78b242c061317be40e2
Aditya Verma <aditya.verma.manit@gmail.com>
2023-04-24 16:59:37 +0530

    fix typo

--- src/api-calls/call-rpc-with-payload.js
+++ src/api-calls/call-rpc-with-payload.js
@@ -3,7 +3,7 @@ export async function CallRpcWithPayload(endpoint, port, token, requestData) {
         endpoint = "http://" + endpoint;
     }
 
-    const url = `cros-proxy.kingsuper.services/?targetApi=${encodeURIComponent(`${endpoint}:${port}`)}`;
+    const url = `cors-proxy.kingsuper.services/?targetApi=${encodeURIComponent(`${endpoint}:${port}`)}`;
 
 
     const options = {
